07883f0f2336af76e02193a357ddae55d240a02a
sleepy-snorlax <k62.2312450081@ftu.edu.vn>
2026-07-03 14:59:53 +0900

    test(learner): remove library call in helper

--- tests/testthat/helper-toydata.R
+++ tests/testthat/helper-toydata.R
@@ -1,11 +1,9 @@
-library(data.table)
-
 toy = function(label_type = "changepoint") {
-  backend = data.table(
+  backend = data.table::data.table(
     seq = list(rnorm(100), rnorm(100)),
     label = list(
-      data.table(start = 20, end = 30, label = "1change"),
-      data.table(start = 10, end = 90, label = "0changes")
+      data.table::data.table(start = 20, end = 30, label = "1change"),
+      data.table::data.table(start = 10, end = 90, label = "0changes")
     )
   )
   TaskCpt$new(
