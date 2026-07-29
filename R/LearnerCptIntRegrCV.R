961eb29db007f574a7516171818c389711630b9a
sleepysnorlax05 <k62.2312450081@ftu.edu.vn>
2026-07-29 14:39:39 +1000

    fix(learner): drop fold.vec, incompatible with resampling

--- R/LearnerCptIntRegrCV.R
+++ R/LearnerCptIntRegrCV.R
@@ -7,12 +7,6 @@ LearnerCptIntRegrCV = R6::R6Class(
         id = "changepoint.intregrcv",
         param_set = ps(
           n.folds = p_int(lower = 2L, tags = "train"),
-          fold.vec = p_uty(
-            tags = "train",
-            custom_check = crate(function(x) {
-              checkmate::check_integerish(x, any.missing = FALSE, null.ok = TRUE)
-            })
-          ),
           verbose = p_int(lower = 0L, upper = 2L, default = 0L, tags = "train"),
           min.observations = p_int(lower = 1L, default = 10L, tags = "train"),
           reg.type = p_fct(c("min", "1sd"), default = "min", tags = "train"),
