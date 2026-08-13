da00eab3ae9ced34548e6339b99dee1e164c23e9
sleepysnorlax05 <k62.2312450081@ftu.edu.vn>
2026-08-13 11:57:18 +1000

    feat(prediction): combine PredictionDataCpt objects across folds

--- R/PredictionDataCpt.R
+++ R/PredictionDataCpt.R
@@ -54,11 +54,37 @@ filter_prediction_data.PredictionDataCpt = function(pdata, row_ids, ...) {
 #' @export
 c.PredictionDataCpt = function(..., keep_duplicates = TRUE) {
   dots = list(...)
-
-  if (length(dots) == 0L) {
-    return(create_empty_prediction_data.TaskCpt())
+  assert_list(dots, "PredictionDataCpt")
+  assert_flag(keep_duplicates)
+  if (length(dots) == 1) {
+    return(dots[[1]])
   }
 
-  assert_list(dots, "PredictionDataCpt")
-  assert()
+  types = map(dots, function(x) {
+    intersect(names(x), names(mlr_reflections$learner_predict_types$changepoint))
+  })
+
+  if (length(unique(types)) > 1) {
+    stopf("Cannot combine PredictionDataCpt objects with different types.")
+  }
+
+  row_ids = unlist(map(dots, "row_ids"))
+  truth = do.call(c, map(dots, "truth"))
+  response = unlist(map(dots, "response"))
+
+  if (!keep_duplicates) {
+    keep = !duplicated(row_ids, fromLast = TRUE)
+    row_ids = row_ids[keep]
+    truth = truth[keep]
+    response = response[keep]
+  }
+
+  pdata = discard(list(row_ids = row_ids, truth = truth, response = response), is.null)
+  class(pdata) = c("PredictionDataCpt", "PredictionData")
+  pdata
+}
+
+#' @export
+as_prediction.PredictionDataCpt = function(x, check = TRUE, ...) {
+  invoke(PredictionCpt$new, check = check, .args = x)
 }
